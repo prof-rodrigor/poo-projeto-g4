5bd166a9fb5b108863c9ed9d06bd6fde59aaf851
Gabriel <gabriel-nunes-gomes-2005@outlook.com>
2024-08-26 21:18:32 -0300

    .

--- src/main/java/br/ufpb/dcx/rodrigor/projetos/participante/model/Participante.java
+++ src/main/java/br/ufpb/dcx/rodrigor/projetos/participante/model/Participante.java
@@ -3,14 +3,141 @@ package br.ufpb.dcx.rodrigor.projetos.participante.model;
 import org.bson.types.ObjectId;
 
 public class Participante {
+
+    // Funcionais
     private ObjectId id;
-    private String nome;
-    private String sobrenome;
-    private String email;
-    private String telefone;
+    private String matricula;
+    private String periodoDeEntrada;
+    private String curso;
+    private String cpf;
+    private String rg;
+    private String orgaoExpedidor;
     private CategoriaParticipante categoria;
 
+    // Contato
+    private String nome;
+    private String sexo;
+    private String sobrenome;
+    private String telefone;
+    private String email;
+    private String instagram;
+    private String linkedIn;
+    private String GitHub;
+    private String endereco;
+
+
+    // Construtor
+
+    public Participante() {
+        this.id = id;
+        this.matricula = matricula;
+        this.periodoDeEntrada = periodoDeEntrada;
+        this.curso = curso;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.orgaoExpedidor = orgaoExpedidor;
+        this.categoria = categoria;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.sobrenome = sobrenome;
+        this.telefone = telefone;
+        this.email = email;
+        this.instagram = instagram;
+        this.linkedIn = linkedIn;
+        this.GitHub = GitHub;
+        this.endereco = endereco;
+    }
+
+
     // Getters and Setters
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getPeriodoDeEntrada() {
+        return periodoDeEntrada;
+    }
+
+    public void setPeriodoDeEntrada(String periodoDeEntrada) {
+        this.periodoDeEntrada = periodoDeEntrada;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getOrgaoExpedidor() {
+        return orgaoExpedidor;
+    }
+
+    public void setOrgaoExpedidor(String orgaoExpedidor) {
+        this.orgaoExpedidor = orgaoExpedidor;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getGitHub() {
+        return GitHub;
+    }
+
+    public void setGithub(String GitHub) {
+        this.GitHub = GitHub;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 
     public ObjectId getId() {
         return id;
@@ -63,12 +190,23 @@ public class Participante {
     @Override
     public String toString() {
         return "Participante{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", sobrenome='" + sobrenome + '\'' +
+                "endereco='" + endereco + '\'' +
+                ", GitHub='" + GitHub + '\'' +
+                ", linkedIn='" + linkedIn + '\'' +
+                ", instagram='" + instagram + '\'' +
                 ", email='" + email + '\'' +
                 ", telefone='" + telefone + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", sexo='" + sexo + '\'' +
+                ", nome='" + nome + '\'' +
                 ", categoria=" + categoria +
+                ", orgaoExpedidor='" + orgaoExpedidor + '\'' +
+                ", rg='" + rg + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", curso='" + curso + '\'' +
+                ", periodoDeEntrada='" + periodoDeEntrada + '\'' +
+                ", matricula='" + matricula + '\'' +
+                ", id=" + id +
                 '}';
     }
 }
